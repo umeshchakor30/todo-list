0d359d9f180ede39c91b350e4a63e8eb8c3fb0cc
umeshchakor30 <umeshchakor30@gmail.com>
2022-01-25 18:49:44 +0530

    Updates

---
+++ precache-manifest.6fc25f912b767e0c76ac43571e98b2ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9053181c869dbe8195797b39af3663db",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "ea1dc7bd01fa8c9321f9",
+    "url": "/todo-list/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b6412378828b2778dc17",
+    "url": "/todo-list/static/js/2.61f38fbf.chunk.js"
+  },
+  {
+    "revision": "1ce896dddecdb4d51806",
+    "url": "/todo-list/static/js/3.c541d426.chunk.js"
+  },
+  {
+    "revision": "ea1dc7bd01fa8c9321f9",
+    "url": "/todo-list/static/js/main.494c8836.chunk.js"
+  },
+  {
+    "revision": "f3294bb86b239d7d3f69",
+    "url": "/todo-list/static/js/runtime~main.13ddd6e0.js"
+  }
+]);
